9fac6734e98b3d52bf44aaaf1f665d3c65241fa5
Cavc13 <13liver@mail.ru>
2022-03-22 18:21:40 +0300

    create @qualifier

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
@@ -1,9 +1,6 @@
 package com.example.dependencyinjectionstart.example2.di
 
-import com.example.dependencyinjectionstart.example2.data.datasource.ExampleLocalDataSource
-import com.example.dependencyinjectionstart.example2.data.datasource.ExampleLocalDataSourceImpl
-import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSource
-import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSourceImpl
+import com.example.dependencyinjectionstart.example2.data.datasource.*
 import dagger.Binds
 import dagger.Module
 
@@ -14,12 +11,20 @@ interface DataModule {
     @ApplicationScope
     @Binds
     fun bindLocalDataSource(
-        localDataSource: ExampleLocalDataSourceImpl
+        impl: ExampleLocalDataSourceImpl
     ): ExampleLocalDataSource
 
+    @ProdQualifier
     @ApplicationScope
     @Binds
     fun bindExampleRemoteDataSource (
-        remoteDataSource: ExampleRemoteDataSourceImpl
+        impl: ExampleRemoteDataSourceImpl
+    ): ExampleRemoteDataSource
+
+    @TestQualifier
+    @ApplicationScope
+    @Binds
+    fun bindTestRemoteDataSource (
+        impl: TestRemoteDataSourceImpl
     ): ExampleRemoteDataSource
 }
